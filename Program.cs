90a9d123b0b75ed54bd55d9a4cb74caf238dacce
tanneratkin <tanneratkin@TANNERATKIN0BAD>
2024-01-17 17:35:25 -0700

    initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Mission_2;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Initialize Variables
+        int numRolls = 0;
+        int[] diceRollCounts = new int[0];
+        RollDice rd = new RollDice();
+
+        // Get Number of Rolls From User
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
+        numRolls = int.Parse(System.Console.ReadLine());
+
+        // Roll Dice
+        diceRollCounts = rd.Roll(numRolls);
+
+        // Output Results
+        System.Console.WriteLine($"\nDICE ROLLING SIMULATION RESULTS\nEach '*' represents 1% of the total number of rolls.\nTotal Number of Rolls = {numRolls}\n");
+        
+        // Iterate Through Values, Printing Each Asterisk as 1% of Total Rolls
+        for (int i = 0; i < diceRollCounts.Length; i++)
+        {
+            int numAsterisks = diceRollCounts[i] * 100 / numRolls;
+            System.Console.WriteLine($"{i + 2}:\t{new string('*', numAsterisks)}");
+        }
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2
+{
+    internal class RollDice
+    {
+        public int[] Roll(int numRolls)
+        {
+            // Initialize Variables
+            int[] diceRollCounts = new int[11];
+            Random random = new Random();
+
+            // Roll Dice
+            for (int i = 0; i < numRolls; i++)
+            {
+                int diceRoll = random.Next(1, 7) + random.Next(1, 7);
+                diceRollCounts[diceRoll - 2]++;
+            }
+
+            // Return Dice Roll Counts Array
+            return diceRollCounts;
+        }
+
+    }
+}
